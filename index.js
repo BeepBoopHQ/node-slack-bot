6f9ce18e21ecf93eade29b57d46def0fd6beb478
justinc <jdcblades@gmail.com>
2016-09-12 19:05:41 -0700

    more listening testing

--- index.js
+++ index.js
@@ -30,18 +30,16 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "#gohawks")
 })
 
-controller.hears(['berto'], ['message_received'], function (bot, message) {
-  bot.reply(message, 'ayo berto')
-})
-
-controller.hears(['go hawks', 'sea'], ['message_received'], function (bot, message) {
+controller.on('message_received', function(bot, message)) {
   if (message === 'sea') {
-    bot.reply(message, 'hawks');
+    bot.reply(message, 'hawks')
     return
   }
 
-  bot.reply(message, '#gohawks')
-
+  if (message === 'berto') {
+    bot.reply(message, 'ayo berto')
+    return
+  }
 })
 
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
