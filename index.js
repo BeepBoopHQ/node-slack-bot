fcbf6dd6fd7ab131e6f2be800ae7b5e5d2ff0c55
justinc <jdcblades@gmail.com>
2016-11-06 17:08:44 -0800

    nothing was wrong

--- index.js
+++ index.js
@@ -548,24 +548,22 @@ function commandFaded(bot, message, commandMsg) {
       return;
     }
 
-    console.log(res);
+    responses = res;
 
   });
 
-  // responses = JSON.parse(responses);
-  //
-  // // get ct - 1 (for id)
-  // var numResponses = Object.keys(responses).length - 1;
-  //
-  // if(commandMsg) {
-  //   // save a message
-  //   responses[numResponses] = commandMsg;
-  //   return;
-  // }
-  //
-  // var num = Math.floor(Math.random() * (numResponses - 1 + 1)) + 1;
-  //
-  // bot.reply(message, responses[num]);
+  // get ct - 1 (for id)
+  var numResponses = Object.keys(responses).length - 1;
+
+  if(commandMsg) {
+    // save a message
+    responses[numResponses] = commandMsg;
+    return;
+  }
+
+  var num = Math.floor(Math.random() * (numResponses - 1 + 1)) + 1;
+
+  bot.reply(message, responses[num]);
   return;
 }
 
