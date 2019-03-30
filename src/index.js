148e957d662649bc55a3f1a4d0663f6ec6fbbfd1
Justin <jdcblades@gmail.com>
2019-03-30 14:29:49 -0700

    removing extra file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-// express for heroku i guess
 const express = require('express');
-
 const Bot = require('../src/bot/bot');
 
 require('dotenv').load();
 
-module.exports.init = () => {
-  // start russ
-  Bot.startBot();
+// start russ
+Bot.startBot();
 
-  // express
-  let app = express();
+// express
+let app = express();
 
-  app.get('/', (req, res) => {
-    res.send('Hi I am a bot');
-  });
+app.get('/', (req, res) => {
+  res.send('Hi I am a bot');
+});
 
-  app.get('/engage-russ-bot.png', (req, res) => {
-    res.sendFile('img/engage-russ-bot.png', { root: __dirname });
-  })
+app.get('/engage-russ-bot.png', (req, res) => {
+  res.sendFile('img/engage-russ-bot.png', { root: __dirname });
+})
 
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('listening on port ', (process.env.PORT || 3000));
-  });
-};
+app.listen(process.env.PORT || 3000, () => {
+  console.log('listening on port ', (process.env.PORT || 3000));
+});
